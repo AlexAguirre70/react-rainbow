8c854c28244557f15a7754c883f1c3dc7d1fd02c
Alex Aguirre <teamaguirre@gmail.com>
2022-09-07 18:05:17 -0700

    Created the intial version of React Rainbow form before changing the useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React , {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
 
 function App() {
-  return (
+  let [colors,setColors]=useState([
+    'violet','blue','lightblue','green','greenyellow','yellow','orange','red'])
+    let colorMap = colors.map((color,i)=>{
+      return(
+        <ColorBlock color={color} key={i} />
+      )
+    
+    }
+  )
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {colorMap}
+      <ColorForm />
     </div>
   );
 }
---
+++ src/ColorBlock.js
@@ -0,0 +1,11 @@
+import { getDefaultNormalizer } from '@testing-library/react'
+import React from 'react'
+
+function ColorBlock (props){
+    return(
+        <div className='colorBlock' style={{'backgroundColor': props.color}}>
+            <p> {props.color}</p>
+        </div>
+    )
+}
+export default ColorBlock
